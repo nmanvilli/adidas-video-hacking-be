d2beba75999421567ee06a023ddb9cbd89fe8fe4
n-manvilli <n.manvilli@gmail.com>
2018-06-20 16:05:17 +0200

    Added base installation

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// Express
+const express = require('express');
+const app = express();
+
+//Random Number
+const rn = require('random-number');
+
+// FS extra
+var fs = require('fs-extra');
+
+// Kew
+var Q = require('kew');
+
+// ???
+var framesArray = [];
